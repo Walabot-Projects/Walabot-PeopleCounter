167d362b1e6b44babbef4bdbd0a09be9de10792f
gal-vayyar <gal.aharoni@vayyar.com>
2016-09-25 10:55:57 +0300

    fixed syntax according to PEP8

--- PeopleCounter.py
+++ PeopleCounter.py
@@ -1,21 +1,24 @@
-from __future__ import print_function, division # python2-python3 compatibillity
-try: input = raw_input # python2-python3 compatibillity
-except NameError: pass # python2-python3 compatibillity
-from datetime import datetime, timedelta # used to the current time
-from math import sin, cos, radians, sqrt # used to calculate MAX_Y_VALUE
+from __future__ import print_function, division
+from datetime import datetime  # used to the current time
+from math import sin, cos, radians, sqrt  # used to calculate MAX_Y_VALUE
 import WalabotAPI as wlbt
+try:
+    input = raw_input
+except NameError:
+    pass
 
-R_MIN, R_MAX, R_RES = 10, 60, 2 # walabot SetArenaR parameters
-THETA_MIN, THETA_MAX, THETA_RES = -10, 10, 10 # walabot SetArenaTheta parameters
-PHI_MIN, PHI_MAX, PHI_RES = -10, 10, 2 # walabot SetArenaPhi parametes
-THRESHOLD = 15 # walabot SetThreshold parametes
+R_MIN, R_MAX, R_RES = 10, 60, 2  # SetArenaR parameters
+THETA_MIN, THETA_MAX, THETA_RES = -10, 10, 10  # SetArenaTheta parameters
+PHI_MIN, PHI_MAX, PHI_RES = -10, 10, 2  # SetArenaPhi parametes
+THRESHOLD = 15  # SetThreshold parametes
 MAX_Y_VALUE = R_MAX * cos(radians(THETA_MAX)) * sin(radians(PHI_MAX))
-SENSITIVITY = 0.25 # amount of seconds to wait after a move has been detected
-TENDENCY_LOWER_BOUND = 0.1 # tendency below that won't count as entrance/exit
-IGNORED_LENGTH = 3 # len in cm to ignore targets in center of arena (each side)
+SENSITIVITY = 0.25  # amount of seconds to wait after a move has been detected
+TENDENCY_LOWER_BOUND = 0.1  # tendency below that won't count as entrance/exit
+IGNORED_LENGTH = 3  # len in cm to ignore targets in center of arena
 
 wlbt.Init()
 wlbt.SetSettingsFolder()
+
 
 def getNumOfPeopleInside():
     """ Gets the current number of people in the room as input and returns it.
@@ -29,6 +32,7 @@ def getNumOfPeopleInside():
         return getNumOfPeopleInside()
     return int(num)
 
+
 def verifyWalabotIsConnected():
     """ Check for Walabot connectivity. loop until detect a Walabot.
     """
@@ -36,11 +40,12 @@ def verifyWalabotIsConnected():
         try:
             wlbt.ConnectAny()
         except wlbt.WalabotError as err:
-            if err.code == 19: # 'WALABOT_INSTRUMENT_NOT_FOUND'
+            if err.code == 19:  # 'WALABOT_INSTRUMENT_NOT_FOUND'
                 input("- Connect Walabot and press 'Enter'.")
         else:
             print('- Connection to Walabot established.')
             return
+
 
 def setWalabotSettings():
     """ Configure Walabot's profile, arena (r, theta, phi), threshold and
@@ -54,6 +59,7 @@ def setWalabotSettings():
     wlbt.SetDynamicImageFilter(wlbt.FILTER_TYPE_NONE)
     print('- Walabot Configurated.')
 
+
 def startAndCalibrateWalabot():
     """ Start the Walabot and calibrate it.
     """
@@ -63,6 +69,7 @@ def startAndCalibrateWalabot():
     while wlbt.GetStatus()[0] == wlbt.STATUS_CALIBRATING:
         wlbt.Trigger()
     print('- Calibration ended.\n- Ready!')
+
 
 def getDataList():
     """ Detect and record a list of Walabot sensor targets. Stop recording
@@ -75,7 +82,6 @@ def getDataList():
     while True:
         wlbt.Trigger()
         targets = wlbt.GetSensorTargets()
-        distance = lambda t: sqrt(t.xPosCm**2 + t.yPosCm**2 + t.zPosCm**2)
         if targets:
             targets = [max(targets, key=distance)]
             numOfFalseTriggers = 0
@@ -88,9 +94,15 @@ def getDataList():
                     numOfFalseTriggers = 0
                 else:
                     numOfFalseTriggers += 1
-            yList = [t.yPosCm for t in targets if abs(t.yPosCm)>IGNORED_LENGTH]
+            yList = [
+                t.yPosCm for t in targets if abs(t.yPosCm) > IGNORED_LENGTH]
             if yList:
                 return yList
+
+
+def distance(t):
+    return sqrt(t.xPosCm**2 + t.yPosCm**2 + t.zPosCm**2)
+
 
 def analizeAndAlert(dataList, numOfPeople):
     """ Analize a given dataList and print to the screen one of two results
@@ -109,11 +121,12 @@ def analizeAndAlert(dataList, numOfPeople):
     elif tendency < 0:
         result = ': Someone has entered!'.ljust(25)
         numOfPeople += 1
-    else: # do not count as a valid entrance / exit
+    else:  # do not count as a valid entrance / exit
         result = ': Someone is at the door!'.ljust(25)
     numToDisplay = ' Currently '+str(numOfPeople)+' people in the room.'
     print(currentTime+result+numToDisplay)
     return numOfPeople
+
 
 def getTypeOfMovement(dataList):
     """ Calculate and return the type of movement detected.
@@ -130,11 +143,14 @@ def getTypeOfMovement(dataList):
     if dataList:
         velocity = getVelocity(dataList)
         tendency = (velocity * len(dataList)) / (2 * MAX_Y_VALUE)
-        bothSides = any(x>0 for x in dataList) and any(x<0 for x in dataList)
+        side1 = any(x > 0 for x in dataList)
+        side2 = any(x < 0 for x in dataList)
+        bothSides = side1 and side2
         aboveLowerBound = abs(tendency) > TENDENCY_LOWER_BOUND
         if bothSides or aboveLowerBound:
             return tendency
     return 0
+
 
 def getVelocity(data):
     """ Calculate velocity of a given set of values using linear regression.
@@ -146,16 +162,19 @@ def getVelocity(data):
     sumY = sumXY = 0
     for x, y in enumerate(data):
         sumY, sumXY = sumY + y, sumXY + x*y
-    if sumXY == 0: return 0 # no values / one values only / all values are 0
-    sumX = x * (x+1) / 2 # Gauss's formula - sum of first x natural numbers
-    sumXX = x * (x+1) * (2*x+1) / 6 # sum of sequence of squares
+    if sumXY == 0:  # no values / one values only / all values are 0
+        return 0
+    sumX = x * (x+1) / 2  # Gauss's formula - sum of first x natural numbers
+    sumXX = x * (x+1) * (2*x+1) / 6  # sum of sequence of squares
     return (sumXY - sumX*sumY/(x+1)) / (sumXX - sumX**2/(x+1))
+
 
 def stopAndDisconnectWalabot():
     """ Stops Walabot and disconnect the device.
     """
     wlbt.Stop()
     wlbt.Disconnect()
+
 
 def PeopleCounter():
     """ Main function. init and configure the Walabot, get the current number
@@ -172,9 +191,11 @@ def PeopleCounter():
         while True:
             dataList = getDataList()
             numOfPeople = analizeAndAlert(dataList, numOfPeople)
-    except KeyboardInterrupt: pass
+    except KeyboardInterrupt:
+        pass
     finally:
         stopAndDisconnectWalabot()
+
 
 if __name__ == '__main__':
     PeopleCounter()
